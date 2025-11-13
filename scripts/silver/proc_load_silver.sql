a7fa6b71e6b9316a7ceed5743b51db849c1314f7
alidev313xA <ali.dev423@gmail.com>
2025-11-13 14:53:52 +0500

    Update proc_load_silver.sql

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -159,22 +159,32 @@ PRINT '>> Truncating Table: silver.api_cust_info';
 TRUNCATE TABLE silver.api_cust_info;
 PRINT '>> Inserting Data Into: silver.api_cust_info';
 INSERT INTO silver.api_cust_info (
-	cst_id, cst_key, cst_firstname, cst_lastname, 
-	cst_marital_status, cst_gndr, cst_create_date
+    cst_id, 
+    cst_key, 
+    cst_firstname, 
+    cst_lastname, 
+    cst_marital_status, 
+    cst_gndr, 
+    cst_create_date, 
+    cst_phone
 )
 SELECT
-	cst_id, cst_key, TRIM(cst_firstname), TRIM(cst_lastname),
-	CASE 
-		WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-		WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-		ELSE 'n/a'
-	END,
-	CASE 
-		WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-		ELSE 'n/a'
-	END,
-	cst_create_date
+    cst_id, 
+    cst_key, 
+    TRIM(cst_firstname), 
+    TRIM(cst_lastname),
+    CASE 
+        WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+        WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+        ELSE 'n/a'
+    END AS cst_marital_status,
+    CASE 
+        WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+        WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+        ELSE 'n/a'
+    END AS cst_gndr,
+    cst_create_date,
+    cst_phone
 FROM bronze.api_customers;
 SET @end_time = GETDATE();
 PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
@@ -302,4 +312,6 @@ BEGIN CATCH
 END CATCH
 END;
 GO
+
+
 
